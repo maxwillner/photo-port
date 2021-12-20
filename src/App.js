998536efdea30c36daa68325093db4b41a4eac22
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-12-20 18:13:40 -0500

    added testing for Gallery and PhotoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
-import './App.css';
-import Nav from "./components/Nav";
-import About from "./components/About";
-import Gallery from "./components/Gallery";
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
   const [categories] = useState([
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects'},
+    {
+      name: 'commercial',
+      description: 'Photos of grocery stores, food trucks, and other commercial projects',
+    },
     { name: 'portraits', description: 'Portraits of people in my life' },
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
@@ -22,7 +24,7 @@ function App() {
         currentCategory={currentCategory}
       ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -3,17 +3,31 @@ import { render, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Nav from "..";
 
+const categories = [
+    { name: 'portraits', description: 'Portraits of people in my life' }
+  ]
+  const mockCurrentCategory = jest.fn();
+  const mockSetCurrentCategory = jest.fn();
+
 afterEach(cleanup);
 
 describe("Nav component", () => {
     //baseline test
-    it("renders", () => {
-        render(<Nav />);
+    it('renders', () => {
+        render(<Nav
+          categories={categories}
+          setCurrentCategory={mockSetCurrentCategory}
+          currentCategory={mockCurrentCategory}
+        />);
     });
 
     //snapshot test
     it("matches snapshot", () => {
-        const { asFragment } = render(<Nav />);
+        const { asFragment } = render(<Nav
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
         // assert value comparison
 
         expect(asFragment()).toMatchSnapshot();
@@ -24,7 +38,11 @@ describe("emoji is visible", () => {
     //baseline test
     it("inserts emoji into the h2", () => {
         //Arrange
-        const { getByLabelText } = render(<Nav />);
+        const { getByLabelText } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
         //Assert
         expect(getByLabelText("camera")).toHaveTextContent("📸");
     });
@@ -33,9 +51,13 @@ describe("emoji is visible", () => {
 describe("links are visible", () => {
     it("inserts text into the links", () => {
         //Arrange
-        const { getByTestId } = render(<Nav />);
+        const { getByTestId } = render(<Nav 
+            categories={categories}
+            setCurrentCategory={mockSetCurrentCategory}
+            currentCategory={mockCurrentCategory}
+        />);
         //Assert
         expect(getByTestId("link")).toHaveTextContent("Oh Snap!");
-        expect(getByTestId("about")).toHaveTextContent("About Me");
+        expect(getByTestId("about")).toHaveTextContent("About me");
     });
 });
